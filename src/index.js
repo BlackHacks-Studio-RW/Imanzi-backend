5f5bfa0e43bf8656f88d4a051867a5fd6459dc54
Innocent Ingabire <inno100os@gmail.com>
2021-01-12 09:53:14 +0200

    changed the database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import 'dotenv/config.js';
 import fileupload from 'express-fileupload';
 import router from './routes/index.js';
@@ -6,6 +7,7 @@ import router from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+connectDB()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(fileupload({
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import usercontroller from '../controllers/user.js';
 import productController from '../controllers/product.js';
-import { userSignInValidate } from '../validators/userSigninValidator';
+import { userSignInValidate } from '../validators/userSigninValidator.js';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req, res) => res.send('Welcome Imanzi Website'));
+router.get('/api/products', asyncHandler(productController.getProducts));
+router.get('/api/products/:id', asyncHandler(productController.getProduct));
 router.post('/api/users/signin', userSignInValidate, usercontroller.signIn);
-router.post('/api/products',productController.createProduct);
+// router.post('/api/products',productController.createProduct);
 
 export default router;
